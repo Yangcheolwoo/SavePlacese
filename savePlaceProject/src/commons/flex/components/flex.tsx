479049b2ff186968eb1359828d8ebf4317ad0a57
Yangcheolwoo <hunlove789@naver.com>
2024-04-15 21:46:04 +0900

    절대 경로 설정 및 esModuleInterop: false 추가(import 사용)

---
+++ savePlaceProject/src/commons/flex/components/flex.tsx
@@ -0,0 +1,5 @@
+export const flex = () => {
+    // return (
+    //     <div
+    // )
+}
---
+++ savePlaceProject/src/login/components/Login.tsx
@@ -0,0 +1,7 @@
+
+export const Login = () => {
+    return (
+        <button>카카오톡 로그인</button>
+    )
+}
+
